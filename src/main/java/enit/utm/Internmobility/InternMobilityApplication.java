c8c5e7ef763171ced56c990d7ac4598e869936c2
Fida BEN ABDALLAH <fbenabdallah1@sofrecom.fr>
2024-04-29 17:14:25 +0100

    first commit

---
+++ src/main/java/enit/utm/Internmobility/InternMobilityApplication.java
@@ -0,0 +1,13 @@
+package enit.utm.Internmobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternMobilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternMobilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enit/utm/Internmobility/InternMobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package enit.utm.Internmobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternMobilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
